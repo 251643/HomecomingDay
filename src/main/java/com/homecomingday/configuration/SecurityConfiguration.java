c503720c5970a34ba90ff9cdffd5a97303ed4126
251643 <251643@naver.com>
2022-08-29 18:06:36 +0900

    리프레시토큰 엔티티 속성명 수정

--- src/main/java/com/homecomingday/configuration/SecurityConfiguration.java
+++ src/main/java/com/homecomingday/configuration/SecurityConfiguration.java
@@ -13,7 +13,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -37,6 +39,7 @@ public class SecurityConfiguration {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
 
   @Bean
   @Order(SecurityProperties.BASIC_AUTH_ORDER)
