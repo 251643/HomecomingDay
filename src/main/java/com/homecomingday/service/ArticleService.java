0193356285f2e1d2b95ce3fbf587c42224518b91
JeongWilly <willy2005@naver.com>
2022-09-14 22:15:35 +0900

    [feat] 검색창 학교별/인기순으로 출력

--- src/main/java/com/homecomingday/service/ArticleService.java
+++ src/main/java/com/homecomingday/service/ArticleService.java
@@ -43,6 +43,77 @@ public class ArticleService {
     }
 
 
+    //검색창 페이지 인기 목록조회
+    public List<GetAllArticleDto> searchPopularArticle(UserDetailsImpl userDetails){
+
+        List<Article> articleList = articleRepository.findBySchoolNameOrderByViewsDesc(userDetails.getMember().getSchoolName());
+        List<GetAllArticleDto> getAllArticleDtoList= new ArrayList<>();
+
+        for(Article findArticle : articleList){
+
+            if (!findArticle.getArticleFlag().equals("calendar")) { //만남일정 부분 제외하고 모든값 출력
+                List<Image> findImage = imageRepository.findAll();
+                List<ImagePostDto> pickImage = new ArrayList<>();
+
+                for (Image image : findImage) {
+                    if (image.getArticle().getId().equals(findArticle.getId())) {
+                        pickImage.add(
+                                ImagePostDto.builder()
+                                        .imageId(image.getId())
+                                        .imgUrl(image.getImgUrl())
+                                        .build()
+                        );
+                    }
+                }
+
+                getAllArticleDtoList.add(
+                        GetAllArticleDto.builder()
+                                .articleId(findArticle.getId())
+                                .title(findArticle.getTitle())
+                                .content(findArticle.getContent())
+                                .imageList(pickImage)
+                                .username(findArticle.getMember().getUsername())
+                                .userImage(changeImage(findArticle.getMember().getUserImage()))
+                                .createdAt(Time.convertLocaldatetimeToTime(findArticle.getCreatedAt()))
+                                .admission(findArticle.getMember().getAdmission().substring(2, 4) + "학번")
+                                .departmentName(findArticle.getMember().getDepartmentName())
+                                .articleFlag(changearticleFlag(findArticle.getArticleFlag()))
+                                .views(findArticle.getViews())
+                                .heartCnt( findArticle.getHeartCnt())
+                                .commentCnt((long) findArticle.getComments().size())
+                                .build()
+                );
+            } else { //만남일정 부분  출력
+                getAllArticleDtoList.add(
+                        GetAllArticleDto.builder()
+                                .articleId(findArticle.getId())
+                                .title(findArticle.getTitle())
+                                .content(findArticle.getContent())
+                                .calendarDate(findArticle.getCalendarDate())
+                                .calendarTime(findArticle.getCalendarTime())
+                                .calendarLocation(findArticle.getCalendarLocation())
+                                .username(findArticle.getMember().getUsername())
+                                .userImage(changeImage(findArticle.getMember().getUserImage()))
+                                .createdAt(Time.convertLocaldatetimeToTime(findArticle.getCreatedAt()))
+                                .admission(findArticle.getMember().getAdmission().substring(2, 4) + "학번")
+                                .departmentName(findArticle.getMember().getDepartmentName())
+                                .articleFlag(changearticleFlag(findArticle.getArticleFlag()))
+                                .views(findArticle.getViews())
+                                .heartCnt(findArticle.getHeartCnt())
+                                .commentCnt((long) findArticle.getComments().size())
+                                .build()
+                );
+            }
+        }
+        return getAllArticleDtoList;
+    }
+
+
+
+
+
+
+
     //검색창 페이지 목록조회
     public List<GetAllArticleDto> searchArticle(UserDetailsImpl userDetails){
         List<Article> articleList = articleRepository.findBySchoolNameOrderByCreatedAtDesc(userDetails.getMember().getSchoolName());
@@ -310,16 +381,21 @@ public class ArticleService {
         articleRepository.save(article);
 
 
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+multipartFile.size());
         if (!articleFlag.equals("calendar")) { //만남일정만 제외하고 이 부분에서 true시에 출력
 
             int checkNum = 1; // 이미지 if(uploadedFile.isEmpty()) 비교를 위해 선언
             // 조건문을 통과하면 안에 값이 비어있다는것, 리스트자체가 아닌 내부 값 자체를 비교해야함
             List<ImagePostDto> imgbox = new ArrayList<>();
+
 //            이미지 null값 비교값
             for (MultipartFile uploadedFile : multipartFile) {
                 if (uploadedFile.isEmpty()) //multipartFile을 비교할때는 isEmpty()를 통해서 비교해야함
+                    System.out.println(uploadedFile.getSize()+">>>>>>>>>>>>>>>>>>>>>>>>uploadedFileSize");
                     checkNum = 0;
             }
+            System.out.println(checkNum+">>>>>>>>>>>>>>>>>>>>>>>>checkNum");
+            System.out.println("정우창>>>>>>>>>2222222222222");
 
 
             if (checkNum==1) { //이미지 있을때 출력 로직
