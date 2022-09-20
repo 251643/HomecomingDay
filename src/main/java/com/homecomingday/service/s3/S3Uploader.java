a81343e7f65b7b418056b5a98a37c6fa0bf7b67f
JeongWilly <willy2005@naver.com>
2022-09-20 16:45:42 +0900

    [fix] s3 이미지 업로드 방식 수정

--- src/main/java/com/homecomingday/service/s3/S3Uploader.java
+++ src/main/java/com/homecomingday/service/s3/S3Uploader.java
@@ -2,13 +2,18 @@ package com.homecomingday.service.s3;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.homecomingday.controller.S3Dto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.imageio.ImageIO;
 import javax.transaction.Transactional;
@@ -17,48 +22,48 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class S3Uploader {
 
 
     private final AmazonS3 amazonS3;
 
-    public S3Uploader(AmazonS3 amazonS3) {
-        this.amazonS3 = amazonS3;
-    }
+    private final AmazonS3Client amazonS3Client;
+
 
     @Value("${cloud.aws.s3.bucket}")
     public String bucket;  // S3 버킷 이름
 
     public S3Dto upload(MultipartFile multipartFile) throws IOException {
-        File uploadFile = convert(multipartFile)  // 파일 변환할 수 없으면 에러
-                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> 파일 변환 실패"));
+        String fileName = UUID.randomUUID() + multipartFile.getOriginalFilename();
 
-        return uploadToS3(uploadFile);
+        String result = amazonS3Client.getUrl(bucket, fileName).toString();
+
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(multipartFile.getSize());
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다.");
+        }
+
+        return new S3Dto(fileName, result);
+
     }
 
-    // S3로 파일 업로드하기
-    @Transactional
-    public S3Dto uploadToS3(File uploadFile) throws IOException {
-
-        String fileName = UUID.randomUUID() + uploadFile.getName();   // S3에 저장될 파일 이름
-
-        String uploadImageUrl = putS3(uploadFile, fileName); // s3로 업로드
-
-        //이미지 리사이징
-//        String resizeFileName=resizeImageFile(uploadImageUrl,fileName);
-
-        removeNewFile(uploadFile);
-        S3Dto s3Dto = new S3Dto(fileName, uploadImageUrl);
 
 
-        return s3Dto;
-    }
-
+    //프로필 이미지 업로드
     public String upload1(MultipartFile multipartFile) throws IOException {
         File uploadFile = convert(multipartFile)  // 파일 변환할 수 없으면 에러
                 .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> 파일 변환 실패"));
@@ -66,7 +71,7 @@ public class S3Uploader {
         return uploadToS31(uploadFile);
     }
 
-    // S3로 파일 업로드하기
+    //프로필 이미지 S3로 파일 업로드하기
     @Transactional
     public String uploadToS31(File uploadFile) throws IOException {
 
@@ -78,13 +83,13 @@ public class S3Uploader {
         return uploadImageUrl;
     }
 
-    // S3로 업로드
+    // 프로필 S3로 업로드
     private String putS3(File uploadFile, String fileName) {
         amazonS3.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
         return amazonS3.getUrl(bucket, fileName).toString();
     }
 
-    // 로컬에 저장된 이미지 지우기
+    // 프로필 등록후 로컬에 저장된 이미지 지우기
     private void removeNewFile(File targetFile) {
         if (targetFile.delete()) {
             log.info("File delete success");
