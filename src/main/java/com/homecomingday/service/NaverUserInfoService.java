bea19ed61578967cea4ae82edc67b2a550312b66
251643 <251643@naver.com>
2022-09-30 00:58:07 +0900

    [refactor] sys out 제거 #142

--- src/main/java/com/homecomingday/service/NaverUserInfoService.java
+++ src/main/java/com/homecomingday/service/NaverUserInfoService.java
@@ -52,7 +52,6 @@ public class NaverUserInfoService {
         requestHeaders.put("Authorization", token);
         String responseBody = get(apiURL,requestHeaders);
 
-        System.out.println(responseBody);
         JSONParser parser = new JSONParser();
         Object obj = parser.parse(responseBody);
         JSONObject jsonObj = (JSONObject) obj;
@@ -60,7 +59,6 @@ public class NaverUserInfoService {
         JSONObject response_obj = (JSONObject)jsonObj.get("response");
         String name = (String)response_obj.get("name");
         String email = (String)response_obj.get("email");
-        System.out.println("로그인한 사람 이름 : " + name);
         System.out.println("로그인한 사람 메일 : " + email);
         NaverMemberInfoDto naverMemberInfoDto = new NaverMemberInfoDto(email, name);
 
