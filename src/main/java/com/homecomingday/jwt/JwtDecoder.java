aa463008f552e9a2ba07ace40f47f5b55c8676cc
251643 <251643@naver.com>
2022-09-29 22:09:59 +0900

    [fix] 헤더 토큰 확인 #127

--- src/main/java/com/homecomingday/jwt/JwtDecoder.java
+++ src/main/java/com/homecomingday/jwt/JwtDecoder.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -23,8 +24,8 @@ import java.util.Map;
 import java.util.Optional;
 
 import static com.homecomingday.jwt.TokenProvider.*;
-@Component
 @RequiredArgsConstructor
+@Configuration
 public class JwtDecoder {
 
 
