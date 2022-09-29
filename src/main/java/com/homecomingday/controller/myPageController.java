838affe434508c5abcda7a0180e85f57055d0157
251643 <251643@naver.com>
2022-09-30 02:01:18 +0900

    [refactor] 게시판 무한스크롤 코드 추가 #142

--- src/main/java/com/homecomingday/controller/myPageController.java
+++ src/main/java/com/homecomingday/controller/myPageController.java
@@ -29,21 +29,6 @@ public class myPageController {
 
     private final MyPageService myPageService;
     private final ArticleRepository articleRepository;
-    @GetMapping("/myPage/myArticle")
-    public ResponseEntity<Slice<MyPageDetailResponseDto>> getArticleScroll(@RequestParam(value = "page") Integer page,
-                                                                           @RequestParam(value = "size") Integer size,
-                                                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
-        Pageable pageable = PageRequest.of(page, size);
-        return ResponseEntity.ok(articleRepository.getArticleScroll(pageable, userDetails));
-    }
-//    @GetMapping("/myPage/myArticle2")
-//    public ResponseEntity<Slice<MyPageDetailResponseDto>> getArticleScroll2(Pageable pageable,
-//                                                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
-//        System.out.println("요청 들어옴");
-//       // Pageable pageable = PageRequest.of(page, size);
-//        return ResponseEntity.ok(articleRepository.getArticleScroll(pageable, userDetails));
-//    }
 
     @GetMapping("/myPage/myArticle2")
     public ResponseEntity<Page<MyPageDetailResponseDto>> getArticleScroll(Pageable pageable, @AuthenticationPrincipal UserDetailsImpl userDetails) {
